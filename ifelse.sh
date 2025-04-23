313eb969e3b5751a95fc7743a821bfdbee4e1dcb
chau-nm <chaunm.146.2001@gmail.com>
2025-04-23 10:53:46 +0700

    chore/insert ifelse

---
+++ ifelse.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ---- is even ----- #
+
+echo -e "\e[1;32m -------------------- NUMBER EVEN CHECK -------------------- \e[0m";
+
+read -p "Enter number: " number;
+
+if [ $((number % 2)) -eq 0 ]
+then
+  echo "$number is even";
+else
+  echo "$number is odd";
+fi
+
+echo -e "\e[1;32m -------------------- FILE EXISTENCE CHECK -------------------- \e[0m";
+
+read -p "Enter filepath: " filepath;
+
+if [ -e "$filepath" ];
+then
+  echo "$filepath exists!"
+else
+  echo "$filepath NOT exists!"
+fi
